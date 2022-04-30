5b2238301d09ed832f63f5ff64debd54d4d402e5
Yoichi Ishigami <104662339+yoichi-i@users.noreply.github.com>
2022-04-30 13:22:05 +0900

    Update streamlit_app.py
    Fixed a typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit
 
-streamlit.title('My Mom/'s New Healthy Diner')
+streamlit.title('My Mom\'s New Healthy Diner')
 
 streamlit.header('Breakfast Menu')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
