892be1c9a37681bdf4fba08f54635911faa1bf31
Yoichi Ishigami <104662339+yoichi-i@users.noreply.github.com>
2022-05-03 21:02:19 +0900

    Update streamlit_app.py
    Changed to display JSON data

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,4 +24,4 @@ streamlit.dataframe(fruits_to_show)
 # New section to display fruityvice api response
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response)
+streamlit.text(fruityvice_response.json())
